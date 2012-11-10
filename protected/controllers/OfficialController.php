f26d63e2ab6ecd8d73152fe1c3167d42825004c3
JohnRudiy <wolfchonok@mail.ru>
2012-11-10 14:08:01 +0200

    Контроллер чиновника

---
+++ protected/controllers/OfficialController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of OfficialController
+ *
+ * @author TJ
+ */
+class OfficialController {
+    //put your code here
+}
+
+?>
